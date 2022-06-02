e17c9e6abb1e333e3cc373ec6a1a877920f3faf0
Leonardo G <69121021+leonardo-ggarcia@users.noreply.github.com>
2022-06-02 20:05:35 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -4,10 +4,10 @@ var modal = "";
 var vidas = 3;
 var acertos = 0;
 var mensagem_acertar = `Parabéns, você descartou corretamente! Descartar corretamente contribuiu para que
-    seu resíduo não fosse destinado no lugar inadequado e não contamine o meio ambiente`
+    seu resíduo não fosse destinado no lugar inadequado e não contaminesse o meio ambiente`
 
-var mensagem_errar = `Infelizmente, você descartou incorretamente! Descartar incorretamente contribui para que
-    seu resíduo vá parar no lugar inadequado e contamine o meio ambiente
+var mensagem_errar = `Infelizmente, você descartou incorretamente! Descartar incorretamente contribuiu para que
+    seu resíduo fosse para o lugar inadequado e contaminesse o meio ambiente
 `;
 
 $(document).ready(function(){
